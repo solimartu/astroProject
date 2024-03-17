d07a599d790f0732125e965750486d3d393b67c8
Solange Marturet <91724897+solimartu@users.noreply.github.com>
2024-03-17 21:17:57 +0100

    first commit to add project from local

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import solidJs from '@astrojs/solid-js';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://astro-frontend-masters.netlify.app',
+	// output: 'hybrid',
+	// experimental: {
+	// 	hybridOutput: true,
+	// },
+	integrations: [
+		react({
+			include: ['**/react/*'],
+		}),
+		solidJs({
+			include: ['**/solid/*'],
+		}),
+	],
+});
